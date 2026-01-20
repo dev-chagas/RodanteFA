4e1df60a0a9d91359b42f496adbc8c87c5afa41c
dev-chagas <fcnj21@gmail.com>
2026-01-20 12:51:21 +0000

    Adicionar pagina inicial

---
+++ pages/index.js
@@ -0,0 +1,8 @@
+function Home () {
+ return <h1>Apreciando cada passo, mas já sonhando com as próximas conquistas que Deus irá derramar.
+    Obrigado mano, e tu é o cara, e se espirrar saúde.!! 
+ </h1>
+ 
+ }
+
+ export default Home;
